openresty/luajit2

commit c844a613df9f2a4e2102d0fa5de827e63ab19281

feature: implemented the new Lua and C API functions for thread exdata.
The Lua API can be used like below:

  local exdata = require "thread.exdata"
  exdata(0xdeadbeefLL)  -- set the exdata of the current Lua thread
  local ptr = exdata()  -- fetch the exdata of the current Lua thread

The exdata value on the Lua land is represented as a cdata object of the
ctype "void*".

Right now the reading API, i.e., `exdata()` calls without any arguments,
can be JIT compiled.

Also exposed the following public C API functions for manipulating
exdata on the C land:

  void lua_setexdata(lua_State *L, void *exdata);
  void *lua_getexdata(lua_State *L);

The exdata pointer is initialized to NULL when the main thread is
created. Any child Lua threads created will inherit the parent's exdata
but still have their own exdata storage. So child Lua threads can always
override the inherited parent exdata pointer values.

This API is used internally by the OpenResty core so never ever mess
with it yourself in the context of OpenResty.

Thanks Zexuan Luo for preparing the final version of the patch.

Signed-off-by: Yichun Zhang (agentzh) <agentzh@gmail.com>

src/lj_obj.h
@@ -661,6 +661,7 @@ struct lua_State {
   GCRef env;		/* Thread environment (table of globals). */
   void *cframe;		/* End of C stack frame chain. */
   MSize stacksize;	/* True stack size (incl. LJ_STACK_EXTRA). */
+  void *exdata;	        /* user extra data pointer. added by OpenResty */
 };
 
 #define G(L)			(mref(L->glref, global_State))

README
@@ -31,6 +31,34 @@ since we still synchronize any upstream changes all the time.
 We introduce our own changes which will never merge or haven't yet merged into
 the upstream LuaJIT (https://github.com/LuaJIT/LuaJIT), which are
 
+feature: implemented the new Lua and C API functions for thread exdata.
+
+    The Lua API can be used like below:
+
+      local exdata = require "thread.exdata"
+      exdata(0xdeadbeefLL)  -- set the exdata of the current Lua thread
+      local ptr = exdata()  -- fetch the exdata of the current Lua thread
+
+    The exdata value on the Lua land is represented as a cdata object of the
+    ctype "void*".
+
+    Right now the reading API, i.e., `exdata()` calls without any arguments,
+    can be JIT compiled.
+
+    Also exposed the following public C API functions for manipulating
+    exdata on the C land:
+
+      void lua_setexdata(lua_State *L, void *exdata);
+      void *lua_getexdata(lua_State *L);
+
+    The exdata pointer is initialized to NULL when the main thread is
+    created. Any child Lua threads created will inherit the parent's exdata
+    but still have their own exdata storage. So child Lua threads can always
+    override the inherited parent exdata pointer values.
+
+    This API is used internally by the OpenResty core so never ever mess
+    with it yourself in the context of OpenResty.
+
 * feature: luajit.h: defined the macro OPENRESTY_LUAJIT for our branch of
   LuaJIT.
 

src/lib_base.c
@@ -35,6 +35,7 @@
 #include "lj_strscan.h"
 #include "lj_strfmt.h"
 #include "lj_lib.h"
+#include "lj_cdata.h"
 
 /* -- Base library: checks ------------------------------------------------ */
 
@@ -652,6 +653,30 @@ static void setpc_wrap_aux(lua_State *L, GCfunc *fn)
   setmref(fn->c.pc, &L2GG(L)->bcff[lj_lib_init_coroutine[1]+2]);
 }
 
+#if LJ_HASFFI
+LJLIB_NOREG LJLIB_CF(thread_exdata) LJLIB_REC(.)
+{
+  ptrdiff_t nargs = L->top - L->base;
+  GCcdata *cd;
+
+  if (nargs == 0) {
+    CTState *cts = ctype_ctsG(G(L));
+    if (cts == NULL)
+      lj_err_caller(L, LJ_ERR_FFI_NOTLOAD);
+    cts->L = L;  /* Save L for errors and allocations. */
+
+    cd = lj_cdata_new(cts, CTID_P_VOID, CTSIZE_PTR);
+    cdata_setptr(cdataptr(cd), CTSIZE_PTR, L->exdata);
+    setcdataV(L, L->top++, cd);
+    return 1;
+  }
+
+  cd = lj_lib_checkcdata(L, 1);
+  L->exdata = cdata_getptr(cdataptr(cd), CTSIZE_PTR);
+  return 0;
+}
+#endif
+
 /* ------------------------------------------------------------------------ */
 
 static void newproxy_weaktable(lua_State *L)
@@ -665,6 +690,13 @@ static void newproxy_weaktable(lua_State *L)
   t->nomm = (uint8_t)(~(1u<<MM_mode));
 }
 
+#if LJ_HASFFI
+static int luaopen_thread_exdata(lua_State *L)
+{
+  return lj_lib_postreg(L, lj_cf_thread_exdata, FF_thread_exdata, "exdata");
+}
+#endif
+
 LUALIB_API int luaopen_base(lua_State *L)
 {
   /* NOBARRIER: Table and value are the same. */
@@ -674,6 +706,11 @@ LUALIB_API int luaopen_base(lua_State *L)
   newproxy_weaktable(L);  /* top-2. */
   LJ_LIB_REG(L, "_G", base);
   LJ_LIB_REG(L, LUA_COLIBNAME, coroutine);
+
+#if LJ_HASFFI
+  lj_lib_prereg(L, LUA_THRLIBNAME ".exdata", luaopen_thread_exdata, env);
+#endif
+
   return 2;
 }
 

src/lj_api.c
@@ -1290,3 +1290,12 @@ LUA_API void lua_setallocf(lua_State *L, lua_Alloc f, void *ud)
   g->allocf = f;
 }
 
+LUA_API void lua_setexdata(lua_State *L, void *exdata)
+{
+  L->exdata = exdata;
+}
+
+LUA_API void *lua_getexdata(lua_State *L)
+{
+  return L->exdata;
+}

src/lj_errmsg.h
@@ -181,6 +181,7 @@ ERRDEF(FFI_CBACKOV,	"too many callbacks")
 #endif
 ERRDEF(FFI_NYIPACKBIT,	"NYI: packed bit fields")
 ERRDEF(FFI_NYICALL,	"NYI: cannot call this C function (yet)")
+ERRDEF(FFI_NOTLOAD,	"ffi module not loaded (yet)")
 #endif
 
 #undef ERRDEF

src/lj_ffrecord.c
@@ -28,6 +28,7 @@
 #include "lj_vm.h"
 #include "lj_strscan.h"
 #include "lj_strfmt.h"
+#include "lj_cdata.h"
 
 /* Some local macros to save typing. Undef'd at the end. */
 #define IR(ref)			(&J->cur.ir[(ref)])
@@ -1110,6 +1111,21 @@ static void LJ_FASTCALL recff_table_clone(jit_State *J, RecordFFData *rd)
   TRef src = J->base[0];
   J->base[0] = lj_ir_call(J, IRCALL_lj_tab_clone, src);
   UNUSED(rd);
+}
+
+/* -- thread library fast functions ------------------------------------------ */
+
+void LJ_FASTCALL recff_thread_exdata(jit_State *J, RecordFFData *rd)
+{
+  TRef tr = J->base[0];
+  if (!tr) {
+    TRef trl = emitir(IRT(IR_LREF, IRT_THREAD), 0, 0);
+    TRef trp = emitir(IRT(IR_FLOAD, IRT_PTR), trl, IRFL_THREAD_EXDATA);
+    TRef trid = lj_ir_kint(J, CTID_P_VOID);
+    J->base[0] = emitir(IRTG(IR_CNEWI, IRT_CDATA), trid, trp);
+    return;
+  }
+  recff_nyiu(J, rd);  /* this case is too rare to be interesting */
 }
 
 /* -- I/O library fast functions ------------------------------------------ */

src/lj_ir.h
@@ -196,6 +196,7 @@ IRFPMDEF(FPMENUM)
   _(FUNC_PC,	offsetof(GCfunc, l.pc)) \
   _(FUNC_FFID,	offsetof(GCfunc, l.ffid)) \
   _(THREAD_ENV,	offsetof(lua_State, env)) \
+  _(THREAD_EXDATA,	offsetof(lua_State, exdata)) \
   _(TAB_META,	offsetof(GCtab, metatable)) \
   _(TAB_ARRAY,	offsetof(GCtab, array)) \
   _(TAB_NODE,	offsetof(GCtab, node)) \

src/lj_lib.c
@@ -301,3 +301,10 @@ int lj_lib_checkopt(lua_State *L, int narg, int def, const char *lst)
   return def;
 }
 
+GCcdata *lj_lib_checkcdata(lua_State *L, int narg)
+{
+  TValue *o = L->base + narg-1;
+  if (!(o < L->top && tviscdata(o)))
+    lj_err_argt(L, narg, LUA_TCDATA);
+  return cdataV(o);
+}

commit 8bd4f1ab505677db1e4a20608ce06a0e9f545cb1

bugfix: we broke the arm build in the commit c844a613. thanks Alec Muffett for the report in #37.
Fix #37.

src/lj_obj.h
@@ -662,6 +662,10 @@ struct lua_State {
   void *cframe;		/* End of C stack frame chain. */
   MSize stacksize;	/* True stack size (incl. LJ_STACK_EXTRA). */
   void *exdata;	        /* user extra data pointer. added by OpenResty */
+#if LJ_TARGET_ARM
+  uint32_t unused1;
+  uint32_t unused2;
+#endif
 };
 
 #define G(L)			(mref(L->glref, global_State))