openresty/luajit2

commit 7c03b051b2d8bd2dbe668332ffb8cddb71685b1b

Make jit.prngstate use the new PRNG code
This change splits out the PRNG so that the PRNG used by the JIT
compiler can be overridden without affecting the PRNG for the
allocator.

The API is changed to reflect the wider PRNG data, which is now 256
bits.  jit.prngstate returns an array with 8 32-bit integers and
similarly, it can be overridden with an array of up to 8 32-bit
integers.

Tests and README.md have been updated to reflect this change.

src/lib_jit.c
@@ -150,9 +150,50 @@ LJLIB_CF(jit_attach)
 
 LJLIB_CF(jit_prngstate)
 {
-  /* XXX Adapt to new pRNG algorithm.  */
-  int32_t cur = 0;
-  setintV(L->top++, cur);
+  GCtab *cur = lj_tab_new(L, 8, 0);
+
+#if LJ_HASJIT
+  jit_State *J = L2J(L);
+
+  /* The old state. */
+  for (int i = 1; i <= 4; i++) {
+    setintV(lj_tab_setint(L, cur, i*2-1), J->prng.u[i-1] & 0xffffffff);
+    setintV(lj_tab_setint(L, cur, i*2), J->prng.u[i-1] >> 32);
+  }
+
+  /* We need to set new state using the input array. */
+  if (L->base < L->top && !tvisnil(L->base)) {
+    GCtab *t = lj_lib_checktab(L, 1);
+    PRNGState prng;
+    int i = 1, len = lj_tab_len(t);
+
+    /* The input array must have at most 8 elements. */
+    if (len > 8)
+      lj_err_arg(L, 1, LJ_ERR_PRNGSTATE);
+
+    for (i = 1; i <= len; i++) {
+      cTValue *v = lj_tab_getint(t, i);
+
+      if (!tvisint(v) && (!tvisnum(v) || (double)(uint32_t)numV(v) != numV(v)))
+        lj_err_arg(L, 1, LJ_ERR_PRNGSTATE);
+
+      if (i & 1)
+	prng.u[(i-1)/2] = numberVint(v);
+      else
+	prng.u[(i-1)/2] = prng.u[(i-1)/2] | ((uint64_t)numberVint(v) << 32);
+    }
+
+    for (i /= 2; i < 4; i++)
+      prng.u[i] = 0;
+
+    /* Re-initialize the JIT prng. */
+    J->prng = prng;
+  }
+#else
+  for (int i = 1; i <= 8; i++)
+    setintV(lj_tab_setint(L, cur, i), 0);
+#endif
+  settabV(L, L->top++, cur);
   return 1;
 }
 

src/lj_errmsg.h
@@ -108,6 +108,7 @@ ERRDEF(NOJIT,	"no JIT compiler for this architecture (yet)")
 ERRDEF(NOJIT,	"JIT compiler permanently disabled by build option")
 #endif
 ERRDEF(JITOPT,	"unknown or malformed optimization flag " LUA_QS)
+ERRDEF(PRNGSTATE,	"PRNG state must be an array with up to 8 integers")
 
 /* Lexer/parser errors. */
 ERRDEF(XMODE,	"attempt to load chunk with wrong mode")

src/lj_jit.h
@@ -472,6 +472,8 @@ typedef struct jit_State {
 
   HotPenalty penalty[PENALTY_SLOTS];  /* Penalty slots. */
   uint32_t penaltyslot;	/* Round-robin index into penalty slots. */
+  PRNGState prng;	/* PRNG state for the JIT compiler, defaults to prng in
+			   global_State. */
 
 #ifdef LUAJIT_ENABLE_TABLE_BUMP
   RBCHashEntry rbchash[RBCHASH_SLOTS];  /* Reverse bytecode map. */

src/lj_mcode.c
@@ -225,7 +225,7 @@ static void *mcode_alloc(jit_State *J, size_t sz)
     }
     /* Next try probing 64K-aligned pseudo-random addresses. */
     do {
-      hint = lj_prng_u64(&J2G(J)->prng) & ((1u<<LJ_TARGET_JUMPRANGE)-0x10000);
+      hint = lj_prng_u64(&J->prng) & ((1u<<LJ_TARGET_JUMPRANGE)-0x10000);
     } while (!(hint + sz < range+range));
     hint = target + hint - range;
   }

src/lj_record.c
@@ -1698,7 +1698,7 @@ static void check_call_unroll(jit_State *J, TraceNo lnk)
       if (lnk) {  /* Possible tail- or up-recursion. */
 	lj_trace_flush(J, lnk);  /* Flush trace that only returns. */
 	/* Set a small, pseudo-random hotcount for a quick retry of JFUNC*. */
-	hotcount_set(J2GG(J), J->pc+1, lj_prng_u64(&J2G(J)->prng) & 15u);
+	hotcount_set(J2GG(J), J->pc+1, lj_prng_u64(&J->prng) & 15u);
       }
       lj_trace_err(J, LJ_TRERR_CUNROLL);
     }

src/lj_trace.c
@@ -312,6 +312,8 @@ void lj_trace_initstate(global_State *g)
   jit_State *J = G2J(g);
   TValue *tv;
 
+  J->prng = g->prng;
+
   /* Initialize aligned SIMD constants. */
   tv = LJ_KSIMD(J, LJ_KSIMD_ABS);
   tv[0].u64 = U64x(7fffffff,ffffffff);
@@ -385,7 +387,7 @@ static void penalty_pc(jit_State *J, GCproto *pt, BCIns *pc, TraceError e)
     if (mref(J->penalty[i].pc, const BCIns) == pc) {  /* Cache slot found? */
       /* First try to bump its hotcount several times. */
       val = ((uint32_t)J->penalty[i].val << 1) +
-	    (lj_prng_u64(&J2G(J)->prng) & ((1u<<PENALTY_RNDBITS)-1));
+	    (lj_prng_u64(&J->prng) & ((1u<<PENALTY_RNDBITS)-1));
       if (val > PENALTY_MAX) {
 	blacklist_pc(pt, pc);  /* Blacklist it, if that didn't help. */
 	return;

t/prngstate.t
@@ -13,17 +13,27 @@ __DATA__
 --- lua
 jit.off()
 
-print(jit.prngstate())
-print(jit.prngstate(32))
-print(jit.prngstate(5617))
-print(jit.prngstate())
+function print_array(a)
+  local out = a[1]
+  for i=2,#a do
+    out = out.." "..tostring(a[i])
+  end
+  print(out)
+end
+
+jit.prngstate({32})
+print_array(jit.prngstate({56,1,7}))
+print_array(jit.prngstate({423,432,432,423,56,867,35,5347}))
+print_array(jit.prngstate())
+print_array(jit.prngstate({423,432,432,423,56,867,35,5347,452}))
 --- out
-0
-0
-32
-5617
+32 0 0 0 0 0 0 0
+56 1 7 0 0 0 0 0
+423 432 432 423 56 867 35 5347
 --- jv
 --- err
+bad argument #1 to 'prngstate' (PRNG state must be an array with up to 8 integers)
+--- exit: 1
 
 
 
@@ -32,7 +42,7 @@ print(jit.prngstate())
 jit.opt.start("minstitch=100000", "hotloop=2")
 
 for i = 1, 50 do
-  jit.prngstate(i)
+  jit.prngstate({i})
 end
 print('ok')
 --- out

commit 29e9e3c3e23412d04631ee46bd4a0fe24b5c8285

bugfix: compilation error "for’ loop initial declarations are only allowed in C99 mode" would happen with older gcc versions like 4.8.
This regression had appeared in commit 7c03b051b2d.

src/lib_jit.c
@@ -150,13 +150,14 @@ LJLIB_CF(jit_attach)
 
 LJLIB_CF(jit_prngstate)
 {
+  int i;
   GCtab *cur = lj_tab_new(L, 8, 0);
 
 #if LJ_HASJIT
   jit_State *J = L2J(L);
 
   /* The old state. */
-  for (int i = 1; i <= 4; i++) {
+  for (i = 1; i <= 4; i++) {
     setintV(lj_tab_setint(L, cur, i*2-1), J->prng.u[i-1] & 0xffffffff);
     setintV(lj_tab_setint(L, cur, i*2), J->prng.u[i-1] >> 32);
   }