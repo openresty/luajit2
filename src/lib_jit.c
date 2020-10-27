openresty/luajit2

commit 29e9e3c3e23412d04631ee46bd4a0fe24b5c8285

bugfix: compilation error "for’ loop initial declarations are only allowed in C99 mode" would happen with older gcc versions like 4.8.
This regression had appeared in commit 7c03b051b2d.

src/lib_jit.c
@@ -150,13 +150,14 @@ LJLIB_CF(jit_attach)
 
 LJLIB_CF(jit_prngstate)
 {
+  int i;
   GCtab *cur = lj_tab_new(L, 8, 0);
 
 #if LJ_HASJIT
   jit_State *J = L2J(L);
 
   /* The old state. */
-  for (int i = 1; i <= 4; i++) {
+  for (i = 1; i <= 4; i++) {
     setintV(lj_tab_setint(L, cur, i*2-1), J->prng.u[i-1] & 0xffffffff);
     setintV(lj_tab_setint(L, cur, i*2), J->prng.u[i-1] >> 32);
   }

commit 29a66f77d27ee669bc16e12f87ea312cd5376aab

bugfix: fixed gcc warnings when -DLUAJIT_DISABLE_JIT is specified.
This regression had appeared in commit 29e9e3c3e23.

src/lib_jit.c
@@ -150,10 +150,10 @@ LJLIB_CF(jit_attach)
 
 LJLIB_CF(jit_prngstate)
 {
-  int i;
   GCtab *cur = lj_tab_new(L, 8, 0);
 
 #if LJ_HASJIT
+  int i;
   jit_State *J = L2J(L);
 
   /* The old state. */