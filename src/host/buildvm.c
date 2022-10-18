9b618a83975497cc820dbd2eb84a6e8423cc25bd
Xiaolin Zhao <zhaoxiaolin@loongson.cn>
2022-10-18 09:15:47 +0800

    LoongArch64: Add support for LuaJIT VM builder
    Co-developed-by: Qiqi Huang <huangqiqi@loongson.cn>

--- src/host/buildvm.c
+++ src/host/buildvm.c
@@ -69,6 +69,8 @@ static int collect_reloc(BuildCtx *ctx, uint8_t *addr, int idx, int type);
 #include "../dynasm/dasm_mips.h"
 #elif LJ_TARGET_S390X
 #include "../dynasm/dasm_s390x.h"
+#elif LJ_TARGET_LOONGARCH64
+#include "../dynasm/dasm_loongarch64.h"
 #else
 #error "No support for this architecture (yet)"
 #endif
