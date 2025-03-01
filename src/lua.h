0efe85a866c0504de97a1de3b0b15852ff648ef5
ensign Curvedhand <eagleivg@users.noreply.github.com>
2025-03-01 12:49:55 +0300

    Added option to collect garbage before timeout expires
    lua_gc now supports LUA_GCTIMEOUT option, which collects all garbage
    it can until selected timeout in microseconds.
    
    Result codes:
    -1 – unsupported platform
    0 – exit by timeout
    1 – garbage collection is fully finished
    
    The option significantly improves FPS stability.

--- src/lua.h
+++ src/lua.h
@@ -231,6 +231,7 @@ LUA_API int  (lua_status) (lua_State *L);
 #define LUA_GCSETPAUSE		6
 #define LUA_GCSETSTEPMUL	7
 #define LUA_GCISRUNNING		9
+#define LUA_GCTIMEOUT		10
 
 LUA_API int (lua_gc) (lua_State *L, int what, int data);
 
